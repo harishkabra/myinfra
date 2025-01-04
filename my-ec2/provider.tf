25b0e9f213de39ce9929af23f6e72bef7b488bd6
Harish Kabra <harishkabra@gmail.com>
2025-01-04 12:44:02 -0500

    first commit

---
+++ my-ec2/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hk-terraform-state-bucket01"
+    key    = "myinfra/myec2-ubuntu/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+
